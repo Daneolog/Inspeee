559ddda2e78ba01b344d5d8149ae6bf1ea7bc2e4
Murtaza <Murch786@gmail.com>
2018-06-26 19:31:25 -0400

    Upload Documents

---
+++ src/app/views/borrower/upload-documents/upload-documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "upload-documents",
+  styleUrls: ["upload-documents.component.scss"],
+  templateUrl: "upload-documents.component.html"
+})
+export class UploadDocumentsComponent {}
