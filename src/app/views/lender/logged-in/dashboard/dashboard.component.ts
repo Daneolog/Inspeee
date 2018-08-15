984220dfac0fdf087711c9919590dc54f0af0ac3
Enoch <daneolog@gmail.com>
2018-08-14 21:54:19 -0400

    Got lots of stuff working

--- src/app/views/lender/logged-in/dashboard/dashboard.component.ts
+++ src/app/views/lender/logged-in/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CacheService } from '../../../../services/cache.service';
 import { UserInfo } from 'src/app/models/user.model';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,9 +11,21 @@ import { UserInfo } from 'src/app/models/user.model';
 export class DashboardComponent implements OnInit {
   userInfo: UserInfo;
 
-  constructor(private cacheService: CacheService) {}
+  accountValue: number;
+
+  constructor(
+    private cacheService: CacheService,
+    private databaseService: DatabaseService
+  ) {}
 
   ngOnInit() {
     this.userInfo = this.cacheService.getStorage('userInfo')[0];
+    this.databaseService
+      .runQuery(
+        `SELECT InvestmentVehicle.LenderID, SUM(AmountInvested) + Balance AS AccountValue FROM InvestmentVehicle JOIN LenderBalance ON InvestmentVehicle.LenderID = LenderBalance.LenderID WHERE InvestmentVehicle.LenderID = ${
+          this.userInfo.UserID
+        }`
+      )
+      .subscribe(data => (this.accountValue = data[0].AccountValue));
   }
 }
